dd366fbadedbb0cfa4cbc41f073af285651d8d1e
Rafael Saca <raffaellsaca@gmail.com>
2023-06-02 22:07:42 -0300

    Add as propriedades com constraints violadas no Probem

--- src/main/java/com/rfsaca/algafood/api/exceptionHandler/Problem.java
+++ src/main/java/com/rfsaca/algafood/api/exceptionHandler/Problem.java
@@ -1,7 +1,7 @@
 package com.rfsaca.algafood.api.exceptionHandler;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
@@ -18,8 +18,15 @@ public class Problem {
     private String type;
     private String title;
     private String detail;
-
     private String userMessage;
     private LocalDateTime timestamp;
+    private List<Field> fields;
+
+    @Getter
+    @Builder
+    public static class Field {
+        private String name;
+        private String userMessage;
+    }
 
 }
