e1aee6eae5ceb67d6d619235b233c7ddc7779019
Rafael Saca <raffaellsaca@gmail.com>
2023-06-23 22:30:21 -0300

    Teste de dados inválidos

--- src/test/java/com/rfsaca/algafood/CadastroCozinhaIntegrationTest.java
+++ src/test/java/com/rfsaca/algafood/CadastroCozinhaIntegrationTest.java
@@ -1,6 +1,9 @@
 package com.rfsaca.algafood;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,4 +31,16 @@ public class CadastroCozinhaIntegrationTest {
         assertNotNull(novaCozinha);
         assertNotNull(novaCozinha.getId());
     }
+
+    @Test
+    public void testarCadastroCozinhaSemNome() {
+        Cozinha novaCozinha = new Cozinha();
+        novaCozinha.setNome(null);
+
+        ConstraintViolationException erroEsperado = assertThrows(ConstraintViolationException.class, () -> {
+            cozinhaService.salvar(novaCozinha);
+        });
+        assertNotNull(erroEsperado);
+    }
+
 }
