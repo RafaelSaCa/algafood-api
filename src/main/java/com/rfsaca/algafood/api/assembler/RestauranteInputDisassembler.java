66cff62b69a4a0554f21a80fb46a223bc0e9d6bc
Rafael Saca <raffaellsaca@gmail.com>
2023-07-18 22:17:10 -0300

    Mapeando para uma instância destino (e não um tipo) com ModelMapper

--- src/main/java/com/rfsaca/algafood/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/rfsaca/algafood/api/assembler/RestauranteInputDisassembler.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.rfsaca.algafood.api.model.input.RestauranteInput;
+import com.rfsaca.algafood.domain.models.Cozinha;
 import com.rfsaca.algafood.domain.models.Restaurante;
 
 @Component
@@ -17,4 +18,11 @@ public class RestauranteInputDisassembler {
         return modelMapper.map(restauranteInput, Restaurante.class);
     }
 
+    public void copyToDomainObject(RestauranteInput restauranteInput, Restaurante restaurante) {
+        // Para evitar org.hibernate.HibernateException: ...model.Cozinha was altered
+        // from 1 to 2
+        restaurante.setCozinha(new Cozinha());
+
+        modelMapper.map(restauranteInput, restaurante);
+    }
 }
--- src/main/java/com/rfsaca/algafood/api/controllers/RestauranteController.java
+++ src/main/java/com/rfsaca/algafood/api/controllers/RestauranteController.java
@@ -73,12 +73,16 @@ public class RestauranteController {
     public RestauranteDto atualizar(@PathVariable Long restauranteId,
             @RequestBody @Valid RestauranteInput restauranteInput) {
         try {
-            Restaurante restaurante = restauranteInputDisassembler.toDomainObject(restauranteInput);
+            // Restaurante restaurante =
+            // restauranteInputDisassembler.toDomainObject(restauranteInput);
 
             Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
 
-            BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro",
-                    "produtos");
+            restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
+
+            // BeanUtils.copyProperties(restaurante, restauranteAtual, "id",
+            // "formasPagamento", "endereco", "dataCadastro",
+            // "produtos");
             return restauranteDtoAssembler.toDto(restauranteService.salvar(restauranteAtual));
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
