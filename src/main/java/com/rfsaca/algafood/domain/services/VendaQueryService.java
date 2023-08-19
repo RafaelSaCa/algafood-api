1350bd8ee27305773193b87eddee593ebdd70cef
Rafael Saca <raffaellsaca@gmail.com>
2023-08-18 22:06:31 -0300

    Tratando time offset na agregação de vendas diárias por data

--- src/main/java/com/rfsaca/algafood/domain/services/VendaQueryService.java
+++ src/main/java/com/rfsaca/algafood/domain/services/VendaQueryService.java
@@ -7,5 +7,5 @@ import com.rfsaca.algafood.domain.models.dto.VendaDiaria;
 
 public interface VendaQueryService {
 
-    List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro);
+    List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset);
 }
--- src/main/java/com/rfsaca/algafood/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/rfsaca/algafood/infrastructure/service/VendaQueryServiceImpl.java
@@ -18,44 +18,50 @@ import com.rfsaca.algafood.domain.services.VendaQueryService;
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
+        @PersistenceContext
+        private EntityManager manager;
 
-    @PersistenceContext
-    private EntityManager manager;
+        @Override
+        public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro, String timeOffset) {
+                var builder = manager.getCriteriaBuilder();
+                var query = builder.createQuery(VendaDiaria.class);
+                var root = query.from(Pedido.class);
+                var predicates = new ArrayList<Predicate>();
 
-    @Override
-    public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
-        var builder = manager.getCriteriaBuilder();
-        var query = builder.createQuery(VendaDiaria.class);
-        var root = query.from(Pedido.class);
+                var functionConvertTzDataCriacao = builder.function(
+                                "convert_tz", Date.class, root.get("dataCriacao"),
+                                builder.literal("+00:00"), builder.literal(timeOffset));
 
-        var functionDateDataCriacao = builder.function(
-                "date", Date.class, root.get("dataCriacao"));
+                var functionDateDataCriacao = builder.function(
+                                "date", Date.class, functionConvertTzDataCriacao);
 
-        var selection = builder.construct(VendaDiaria.class,
-                functionDateDataCriacao,
-                builder.count(root.get("id")),
-                builder.sum(root.get("valorTotal")));
+                var selection = builder.construct(VendaDiaria.class,
+                                functionDateDataCriacao,
+                                builder.count(root.get("id")),
+                                builder.sum(root.get("valorTotal")));
 
-        // FILTROS NA CONSULTA
-        var predicates = new ArrayList<Predicate>();
+                if (filtro.getRestauranteId() != null) {
+                        predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+                }
 
-        if (filtro.getRestauranteId() != null) {
-            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+                if (filtro.getDataCriacaoInicio() != null) {
+                        predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"),
+                                        filtro.getDataCriacaoInicio()));
+                }
+
+                if (filtro.getDataCriacaoFim() != null) {
+                        predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"),
+                                        filtro.getDataCriacaoFim()));
+                }
+
+                predicates.add(root.get("status").in(
+                                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
+                query.select(selection);
+                query.groupBy(functionDateDataCriacao);
+                query.where(predicates.toArray(new Predicate[0]));
+
+                return manager.createQuery(query).getResultList();
         }
-        if (filtro.getDataCriacaoInicio() != null) {
-            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoInicio()));
-        }
-        if (filtro.getDataCriacaoFim() != null) {
-            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"), filtro.getDataCriacaoFim()));
-        }
-        predicates.add(root.get("status").in(
-                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
-
-        query.select(selection);
-        query.groupBy(functionDateDataCriacao);
-        query.where(predicates.toArray(new Predicate[0]));
-
-        return manager.createQuery(query).getResultList();
-    }
 
 }
