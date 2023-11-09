1c4d47f238296c9db739f824388a6ab2635a780a
Rafael Saca <raffaellsaca@gmail.com>
2023-11-09 12:47:51 -0300

    Processando template do corpo de e-mails com Apache FreeMarker

--- src/main/java/com/rfsaca/algafood/domain/services/FluxoPedidoService.java
+++ src/main/java/com/rfsaca/algafood/domain/services/FluxoPedidoService.java
@@ -24,7 +24,9 @@ public class FluxoPedidoService {
 
         var mensagem = Mensagem.builder()
                 .assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado!")
-                .corpo("Seu pedido <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+                .corpo("pedido-confirmado.html")
+                .variavel("pedido", pedido)
+
                 .destinatario(pedido.getCliente().getEmail())
                 .build();
 
